7b395a2164b42ddbbb81729dae3e8f4d3df2c8d2
0verLighT <alexandre@0verlight.com>
2026-04-28 03:39:48 +0200

    feat: use flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake cub3d";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          nativeBuildInputs = with pkgs; [
+            meson
+            ninja
+            pkg-config
+            lldb_22
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            valgrind
+            gdb
+          ];
+          shellHook = ''cc --version'';
+        };
+      }
+    );
+}
