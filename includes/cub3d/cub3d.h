59cb3b53050cb7c176300b92faaf7fb95c21e948
0verLighT <alexandre@0verlight.com>
2026-04-28 03:43:56 +0200

    feat: check if the path is valid

--- includes/cub3d/cub3d.h
+++ includes/cub3d/cub3d.h
@@ -13,4 +13,9 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
 #endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,40 @@
 /*   By: amartel <amartel@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/18 02:11:15 by amartel           #+#    #+#             */
-/*   Updated: 2026/04/18 03:26:32 by amartel          ###   ########.fr       */
+/*   Updated: 2026/04/28 02:50:09 by amartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d/cub3d.h"
 
-int	main(void)
+int	ft_valid_path(char *filename)
 {
+	int	fd;
+
+	if (!ft_strnstr(filename + ft_strlen(filename) - 4, ".cub", 4))
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	return (fd);
+}
+
+int	main(int ac, char **av)
+{
+	int	fd;
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	fd = ft_valid_path(av[1]);
+	close(fd);
 	return (0);
 }
